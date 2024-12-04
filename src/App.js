256688e1a9518b52a75978c78e74dc6bac13ca97
Muzamil <muzixtra@gmail.com>
2024-12-04 14:27:59 +0500

    after setting the url

--- src/App.js
+++ src/App.js
@@ -62,7 +62,7 @@ export default function App() {
           setIsLoading(true);
           setError("");
 
-          const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`, {
+          const res = await fetch(`https://www.omdbapi.com/?apikey=${KEY}&s=${query}`, {
             signal: controller.signal
           });
           if (!res.ok) throw new Error("Something went wrong ");
